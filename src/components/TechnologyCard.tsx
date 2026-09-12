fe2f655c3c643976878205fe6b7809d46fded946
Arabi2602 <ahbabwebdevelopment@gmail.com>
2026-09-12 13:03:50 +0600

    Updated Style

--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -1,3 +1,4 @@
+
 import type { ITechonologi } from '../type/techonologiType';
 
 interface TechnologyCardProps {
@@ -13,30 +14,34 @@ const TechnologyCard = ({
 }: TechnologyCardProps) => {
 
     return (
-        <div >
-            <div>
+        <div className="card bg-base-100 border border-gray-200 shadow-sm">
 
-                <div >
+            <div className="flex items-center justify-between px-6 pt-6">
+
+                <div className="w-12 h-12 flex items-center justify-center">
                     <img
                         src={technology.icon}
                         alt={technology.name}
                         className="w-10 h-10"
                     />
                 </div>
-                <span>
+
+                <span className="badge badge-info badge-outline">
                     {technology.badge}
                 </span>
 
             </div>
-            <div >
-                <h2>
+            <div className="card-body pt-4">
+
+                <h2 className="card-title text-xl">
                     {technology.name}
                 </h2>
                 <p className="text-gray-500 text-sm leading-6">
                     {technology.description}
                 </p>
+
                 <div className="divider my-2"></div>
-                <div>
+                <div className="flex items-center justify-between text-sm">
 
                     <span className="badge badge-ghost">
                         {technology.category}
@@ -54,6 +59,7 @@ const TechnologyCard = ({
                     </span>
 
                 </div>
+
                 <div className="card-actions mt-4">
 
                     <button
